4467a31dad3a19b48e230a6370fd76b04f468f5e
Tnze <cjd001113@outlook.com>
2019-06-24 00:21:31 +0800

    调整注释文档格式

--- cqp/events.go
+++ cqp/events.go
@@ -40,10 +40,10 @@ var Enable func() int32
 var Disable func() int32
 
 // PrivateMsg 在收到私聊消息时被调用
-// - subType	子类型，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组
-// - msgId		消息ID
-// - fromQQ		来源QQ
-// - msg		消息内容
-// - font		字体
+//	- subType	子类型，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组
+//	- msgId		消息ID
+//	- fromQQ	来源QQ
+//	- msg		消息内容
+//	- font		字体
 // 返回非零值,消息将被拦截,最高优先不可拦截
 var PrivateMsg func(subType, msgID int32, fromQQ int64, msg string, font int32) int32
