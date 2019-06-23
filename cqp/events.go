eaef01d5cf755687eb80ce7536d8860c60129c16
Tnze <cjd001113@outlook.com>
2019-06-24 00:28:16 +0800

    修改文档

--- cqp/events.go
+++ cqp/events.go
@@ -39,11 +39,7 @@ var Enable func() int32
 // Disable 在插件禁用时被调用
 var Disable func() int32
 
-// PrivateMsg 在收到私聊消息时被调用
-//	- subType	子类型，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组
-//	- msgId		消息ID
-//	- fromQQ	来源QQ
-//	- msg		消息内容
-//	- font		字体
-// 返回非零值,消息将被拦截,最高优先不可拦截
+// PrivateMsg 在收到私聊消息时被调用。
+// subType为子类型，可选的值有，11/来自好友 1/来自在线状态 2/来自群 3/来自讨论组。
+// 若返回非0值，消息将被拦截,最高优先不可拦截。
 var PrivateMsg func(subType, msgID int32, fromQQ int64, msg string, font int32) int32
